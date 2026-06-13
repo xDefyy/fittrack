2fcfbf43d34f7d5831ddc64e1a9f3b7fbc332b7d
Kader Mayora <kadermayora@gmail.com>
2026-06-13 16:05:03 +0200

    change localhost to docker up

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="FitTrack API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost"],
+    allow_origins=["http://localhost:5173", "http://localhost", "http://localhost:80"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
