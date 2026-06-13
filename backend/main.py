9183f40e283c74faef1b906e47f6f4459ec303e0
toniopas <antoine.pasqualini31@gmail.com>
2026-06-13 15:58:18 +0200

    push

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI(title="FitTrack API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
